7d0ddc5b82917f788c0bfddb76766eada4d07df9
VertexF <danmanley93@gmail.com>
2020-11-29 09:31:13 +0000

    Documented the entry point.

--- src/Tempest/Tempest/EntryPoint.h
+++ src/Tempest/Tempest/EntryPoint.h
@@ -5,10 +5,17 @@
 
 #include "Application.h"
 
+//Currently only supports Windows.
 #ifdef WIN32
 
+//This function is global so the user can implement their own application 
+//without having the write the main function. Although globals are ugly this 
+//allows the engine to control how to main function is written.
 extern Tempest::Application* Tempest::createApplication();
 
+//Here set up the logging function. Then start then engine and run it.
+//We also want to control how the life of the engine so it's not a 
+//smart pointer.
 int main(int argc, char** argv) 
 {
     Tempest::Log::init();
